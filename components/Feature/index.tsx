56897cf9d2d90e19a6152517320a6eab893fb4c1
Mateo Olarte <mateo.olarte8@gmail.com>
2021-06-05 21:01:38 -0500

    fix typescript prop

--- components/Feature/index.tsx
+++ components/Feature/index.tsx
@@ -2,7 +2,10 @@ import { ReactElement } from 'react';
 import { FeatureProps } from './interfaces';
 import Link from 'next/link';
 
-export default function Feature({ children, link }: FeatureProps): ReactElement {
+export default function Feature({
+  children,
+  link,
+}: FeatureProps): ReactElement {
   return (
     <Link href={link}>
       <a className="border-2 border-gray-300 border-opacity-50 p-2 grid grid-cols-1 grid-rows-2 justify-items-center items-center cursor-pointer hover:bg-gray-300 hover:bg-opacity-10 hover:shadow hover:border-transparent">
--- components/Feature/interfaces.ts
+++ components/Feature/interfaces.ts
@@ -1,4 +1,4 @@
 export interface FeatureProps {
   children: object | string;
-  link?: 'string';
+  link?: string;
 }
