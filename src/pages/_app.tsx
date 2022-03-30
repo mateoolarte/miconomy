88deade8d6c30273781aead4f69adf00156ed12a
Mateo Olarte <mateo.olarte8@gmail.com>
2022-03-30 12:42:39 -0500

    add ant design as design system framework

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../libs/apollo';
 
+import 'antd/dist/antd.variable.min.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.apolloStore);
 
